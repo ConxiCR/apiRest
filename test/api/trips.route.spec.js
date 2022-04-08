12ae009fd417b5a4da5e9189d70c29d44f8c4a2f
ConxiCr <conxicr@hotmail.com>
2022-04-08 12:48:19 +0200

    post
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- test/api/trips.route.spec.js
+++ test/api/trips.route.spec.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const mongoose = require('mongoose');
 const app = require('../../app');
+const Trip = require('../../models/trip.model');
 
 describe('Pruebas sobre la API de trips', () => {
 
@@ -13,7 +14,7 @@ describe('Pruebas sobre la API de trips', () => {
         await mongoose.disconnect();
     });
 
-    //rutas: 1ªGET
+    //rutas: 1ª GET
     describe('GET/api/trips', () => {
 
         let response;
@@ -31,5 +32,28 @@ describe('Pruebas sobre la API de trips', () => {
             expect(response.body).toBeInstanceOf(Array);
         });
     });
+    //2ª POST
+    describe('POST/api/trips', () => {
 
+        const newTrip = {name: 'test trip', destination: 'Berlín', category:'friends', star_date: '2022-06-20' }
+
+        /*afterAll(async() => {
+            await Trip.deleteMany({name:'test trip'});
+        })*/
+        //1ª prueba. Ruta funciona
+        it('La ruta funciona', async () => {
+            const response = await request(app).post('/api/trips').send(newTrip);
+            
+            expect(response.status).toBe(200);
+            expect(response.headers['content-type']).toContain('json');
+        });
+        //2ª prueba
+        it('Se inserta correctamente', async() => {
+            
+            const response = await request(app).post('/api/trips').send(newTrip);
+            expect(response.body._id).toBeDefined();
+            expect(response.body.name).toBe(newTrip.name);
+        
+        });
+    });
 });
